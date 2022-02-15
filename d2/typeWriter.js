2895a104f178264fca37be24d3fe8cc860a17567
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-15 23:58:48 +0000

    git practice

--- d2/typeWriter.js
+++ d2/typeWriter.js
@@ -5,4 +5,3 @@ for (let char = 0, ms = 0; char < sentence.length; char++, ms += 200){
   setTimeout(()=> {process.stdout.write(sentence[char])}, ms);
 }
 
-//SHA256:vzDQPNcJjkT/cZn3vo1cVAu/y7Cs9c+A4XJFspv4H4w
