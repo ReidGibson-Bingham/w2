8fbd61d051bc124859d235bdcd32d0ad39b4ebf8
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-17 01:16:01 +0000

    pair programming project

---
+++ d3-net/net_example/client.js
@@ -0,0 +1,6 @@
+const conn = net.createConnection({ 
+  host: 'localhost', // change to IP address of computer or ngrok host if tunneling
+  port: 3000 // or change to the ngrok port if tunneling
+});
+
+conn.setEncoding('utf8'); // interpret data as text
